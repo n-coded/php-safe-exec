b01d3af7783b3106380b19fb6161fe3f60657b52
Tran Phuong Ngoc <ngoctp@fsoft.com.vn>
2015-09-28 07:14:42 +0700

    First version of SafeExec

---
+++ src/Ncoded/SafeExec/SafeExec.php
@@ -0,0 +1,79 @@
+<?php namespace Ncoded\SafeExec;
+
+class SafeExec {
+	protected $command;
+	protected $workingDirectory;
+	
+	function __construct($command) {
+		$this->setCommand($command);
+	}
+	
+	protected setCommand($command) {
+		$this->command = escapeshellcmd($command);
+		return $this;
+	}
+	
+	public function setWorkingDirectory($path) {
+		$this->workingDirectory = $path;
+		return $this;
+	}
+	
+	public function addArgument($argument, $value = null, $separator = ' ') {
+		$this->command .= ' ' . $argument;
+		if (!is_null($value)) {
+			$this->command .= $separator . escapeshellarg($value);
+		}
+		return $this;
+	}
+	
+	public function addValue($value) {
+		$this->command .= ' ' . escapeshellarg($value);
+		return $this;
+	}
+	
+	public function outputTo($file) {
+		$this->command .= ' > ' . escapeshellarg($file);
+		return $this;
+	}
+	
+	public function errorTo($file) {
+		$this->command .= ' 2> ' . escapeshellarg($file);
+		return $this;
+	}
+	
+	public function stdoutToNull() {
+		$this->command .= ' > /dev/null';
+		return $this;
+	}
+	
+	public function stderrToNull() {
+		$this->command .= ' 2> /dev/null';
+		return $this;
+	}
+	
+	public function stderrToStdout() {
+		$this->command .= ' 2>&1';
+		return $this;
+	}
+	
+	protected function getFullCommand() {
+		return (is_null($this->workingDirectory) ? '' : 'cd ' . escapeshellarg($this->workingDirectory) . ' && ') . $this->command;
+	}
+	
+	public function exec(&$output = null, &$return_var = null) {
+		return exec($this->getFullCommand(), $output, $return_var);
+	}
+	
+	public function shell_exec() {
+		return shell_exec($this->getFullCommand());
+	}
+	
+	public function system(&$return_var = null) {
+		return system($this->getFullCommand(), $return_var);
+	}
+	
+	public function passthru(&$return_value = null) {
+		return passthru($this->getFullCommand(), $return_value);
+	}
+	
+}
